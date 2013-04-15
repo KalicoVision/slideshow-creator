be9c2c5ae0d18573763c8b5b4eec0db3cd320cb7
KalicoVision <lokinian@gmail.com>
2013-04-15 12:16:44 -0700

    Initial Check-in

---
+++ slideshow-creator.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Slideshow Creator
+Plugin URI: http://2bcoding.com/wpplugins/slideshow-creator
+Description: Create and display slide shows from photos or blog posts.
+Version: 0.1
+Author: 2BCoding
+Author URI: http://2bcoding.com
+License: GPL2
+*/
